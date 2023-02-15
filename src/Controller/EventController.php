39f02b1341b45bf210a467a9a639e01e64137e77
Eddy <eddy@qdata-mg.com>
2023-02-15 16:07:59 +0300

    creation route detail event

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 use App\Repository\EventsRepository;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -17,5 +18,16 @@ class EventController extends AbstractController
         $jsonEventList = $serializer->serialize($eventList, 'json');
         return new JsonResponse($jsonEventList, Response::HTTP_OK, [], true);
     }
+
+    #[Route('/api/events/{id}', name: 'detailEvent', methods: ['GET'])]
+    public function getDetailEvent(int $id, SerializerInterface $serializer, EventsRepository $EventRepository): JsonResponse 
+    {
+        $Event = $EventRepository->find($id);
+        if ($Event) {
+            $jsonEvent = $serializer->serialize($Event, 'json');
+            return new JsonResponse($jsonEvent, Response::HTTP_OK, [], true);
+        }
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+   }
 
 }

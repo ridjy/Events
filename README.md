8039dbf7f08a4d55183ebf0cefbf75b231ae1b83
Eddy <eddy@qdata-mg.com>
2023-02-15 16:58:44 +0300

    create event OK

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ Ajouter des fixtures
 php bin/console doctrine:fixtures:load
 
 Utiliser paramConverter
+
+POST creation event
+{
+"nom":"mon évènement",
+"date_debut":"2023-06-03",
+"date_fin":"2023-06-03",
+"nbr_max_participants":25
+}
--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -6,9 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\EventsRepository;
+use App\Entity\Events;
 use Symfony\Component\Serializer\SerializerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class EventController extends AbstractController
 {
@@ -18,7 +21,7 @@ class EventController extends AbstractController
         $eventList = $eventRepository->findAll();
         $jsonEventList = $serializer->serialize($eventList, 'json');
         return new JsonResponse($jsonEventList, Response::HTTP_OK, [], true);
-    }
+    }//fin function lister les évènements
 
     #[Route('/api/events/{id}', name: 'detailEvent', methods: ['GET'])]
     public function getDetailEvent(int $id, SerializerInterface $serializer, EventsRepository $EventRepository): JsonResponse 
@@ -29,6 +32,21 @@ class EventController extends AbstractController
             return new JsonResponse($jsonEvent, Response::HTTP_OK, [], true);
         }
         return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+   }//fin function affichage détail évènement
+
+   #[Route('/api/events', name:"createEvent", methods: ['POST'])]
+    public function createEvent(Request $request, SerializerInterface $serializer, ManagerRegistry $doctrine, UrlGeneratorInterface $urlGenerator): JsonResponse 
+    {
+        $Event = $serializer->deserialize($request->getContent(), Events::class, 'json');
+        $em = $doctrine->getManager();
+        $em->persist($Event);
+        $em->flush();
+
+        $jsonEvent = $serializer->serialize($Event, 'json', ['groups' => 'getEvents']);
+        
+        $location = $urlGenerator->generate('detailEvent', ['id' => $Event->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonEvent, Response::HTTP_CREATED, ["Location" => $location], true);
    }
 
    #[Route('/api/events/{id}', name: 'deleteEvent', methods: ['DELETE'])]
@@ -40,6 +58,6 @@ class EventController extends AbstractController
         $em->flush();
 
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
-    }
+    }//fin function supprimr évènement
 
 }

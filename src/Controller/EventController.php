30391657b892a7cff742d6b76713ceeaea499c4f
Eddy <eddy@qdata-mg.com>
2023-02-15 16:02:06 +0300

    installation et utilisation serializer

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -5,15 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\EventsRepository;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class EventController extends AbstractController
 {
-    #[Route('/event', name: 'app_event')]
-    public function index(): JsonResponse
+    #[Route('/api/events', name: 'events', methods: ['GET'])]
+    public function getEventList(EventsRepository $eventRepository, SerializerInterface $serializer): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/EventController.php',
-        ]);
+        $eventList = $eventRepository->findAll();
+        $jsonEventList = $serializer->serialize($eventList, 'json');
+        return new JsonResponse($jsonEventList, Response::HTTP_OK, [], true);
     }
+
 }
